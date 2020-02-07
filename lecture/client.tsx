035201e45d04aa42fe8534d491d59b8bdf0965de
hbs <hbsowo58@gmail.com>
2020-02-07 11:06:33 +0900

    edit client tsx import & edit README.md

--- lecture/client.tsx
+++ lecture/client.tsx
@@ -3,6 +3,8 @@ import * as ReactDOM from "react-dom";
 import { hot } from "react-hot-loader";
 
 // import GuGuDan from "./GuGuDan";
+// import GuGuDan from "./GuGuDanClass";
+// import WordRelay from "./WordRelay";
 import WordRelay from "./WordRelayClass";
 
 const Hot = hot(WordRelay);
